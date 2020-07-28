85072b95775d8f9fdce7e3250622bcd59890d964
Jagriti Srivastava <jagritisrvstv@gmail.com>
2020-07-28 09:12:16 +0545

    index.php
    basic initialisation of php

---
+++ index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head><meta charset="utf-8" /></head><body>
+	
+<?php
+echo "My first PHP script!";
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+
+?>
+
+</body></html>
